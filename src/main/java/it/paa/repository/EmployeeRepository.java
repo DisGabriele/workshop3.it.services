aa0370e70ab5240ba2f99228cb3bdacded7f1aca
Gabriele <gabriele.distinti@gmail.com>
2024-05-21 12:12:18 +0200

    forgot to push the repositories;

--- src/main/java/it/paa/repository/EmployeeRepository.java
+++ src/main/java/it/paa/repository/EmployeeRepository.java
@@ -1,6 +1,7 @@
 package it.paa.repository;
 
 import it.paa.model.entity.Employee;
+import jakarta.validation.ConstraintViolationException;
 import jakarta.ws.rs.NotFoundException;
 import jakarta.ws.rs.core.NoContentException;
 
@@ -10,7 +11,7 @@ import java.util.List;
 public interface EmployeeRepository {
     List<Employee> getAll(String surname, LocalDate startDate, LocalDate endDate) throws NoContentException,IllegalArgumentException;
     Employee getById(Long id) throws NotFoundException;
-    Employee save(Employee employee);
-    Employee update(Employee employee);
+    Employee save(Employee employee) throws ConstraintViolationException;
+    Employee update(Employee employee) throws ConstraintViolationException;
     void delete(Long id) throws NotFoundException;
 }
--- src/main/java/it/paa/repository/RoleRepository.java
+++ src/main/java/it/paa/repository/RoleRepository.java
@@ -2,6 +2,7 @@ package it.paa.repository;
 
 import it.paa.model.entity.Role;
 import jakarta.persistence.PersistenceException;
+import jakarta.validation.ConstraintViolationException;
 import jakarta.ws.rs.NotFoundException;
 import jakarta.ws.rs.core.NoContentException;
 
@@ -10,7 +11,7 @@ import java.util.List;
 public interface RoleRepository {
     List<Role> getAll(String name, Float min_salary) throws NoContentException;
     Role getById(Long id) throws NotFoundException;
-    Role save(Role role) throws PersistenceException;
-    Role update(Role role);
+    Role save(Role role) throws PersistenceException, ConstraintViolationException;
+    Role update(Role role) throws PersistenceException, ConstraintViolationException;
     void delete(Long id) throws NotFoundException;
 }
